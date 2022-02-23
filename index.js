569bb92f9b5ba435fc2a29ccc34f205dd80c3858
AVENKATASIVASANKAR <97689510+AVENKATASIVASANKAR@users.noreply.github.com>
2022-02-23 13:16:52 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+function callname(){  
+var name1=prompt("Enter player1 name");
+document.querySelector("#player1").innerHTML=name1;
+return result;
+}
+function callname2(){
+var name2=prompt("Enter player2 name");
+document.querySelector("#player2").innerHTML=name2;
+return result;
+}
 
-
+function game(){
 var randomNumber1 = Math.random()*6+1;
 randomNumber1 = Math.floor(randomNumber1);
 var randomDiceimage = "dice"+randomNumber1+".png";
@@ -14,7 +24,8 @@ var randomDiceimage2="dice"+randomNumber2+".png";
 var randomDicesource="images/"+randomDiceimage2;
 var image2=document.querySelectorAll("img")[1];
 image2.setAttribute("src",randomDicesource);
-
+return result;
+}
 
 if (randomNumber1 > randomNumber2){
     document.querySelector("h1").innerHTML="Player1 won";
