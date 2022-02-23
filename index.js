8d889570bceeaeac1d60bf168cbdeaa5830f4eaf
AVENKATASIVASANKAR <97689510+AVENKATASIVASANKAR@users.noreply.github.com>
2022-02-23 14:09:34 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 function callname(){  
 var name1=prompt("Enter player1 name");
+name1= name1.slice(0,1).toUpperCase()+name1.slice(1,name1.length).toLowerCase();
 document.querySelector("#player1").innerHTML=name1;
 return result;
 }
 function callname2(){
 var name2=prompt("Enter player2 name");
+name2= name2.slice(0,1).toUpperCase()+name2.slice(1,name2.length).toLowerCase();
 document.querySelector("#player2").innerHTML=name2;
 return result;
 }
